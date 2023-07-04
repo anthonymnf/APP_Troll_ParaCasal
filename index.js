80de6af50559288a0ae680aac69cdbf4e29aaac2
Anthony Matheus N. Freitas <115318191+anthonymnf@users.noreply.github.com>
2023-07-04 16:36:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$("#change").click(function () {
+  // $("#change").html("sim");
+  $(".surpresa").show();
+});
+
+$("#move").hover(function () {
+  change();
+});
+
+function change() {
+  var i = Math.floor(Math.random() * 500) + 1;
+  var j = Math.floor(Math.random() * 100) + 1;
+  $("#move").css({ top: j + "px", left: i + "px" });
+}
